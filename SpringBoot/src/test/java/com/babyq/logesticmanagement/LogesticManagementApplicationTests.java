77a5a28f6c6d3e0aa5d9d3fb20134026efcf06c7
Kita-71 <842935300@qq.com>
2022-11-13 16:38:05 +0800

    ProjectMake

---
+++ SpringBoot/src/test/java/com/babyq/logesticmanagement/LogesticManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babyq.logesticmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogesticManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
