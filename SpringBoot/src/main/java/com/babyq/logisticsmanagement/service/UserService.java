ff2c5b9a2a0a01d5f600d00959859509d76cf7ca
rayyxiao <1340995873@qq.com>
2022-11-28 09:38:55 +0800

    物流操作页面CourierOper拉取后debug

--- SpringBoot/src/main/java/com/babyq/logisticsmanagement/service/UserService.java
+++ SpringBoot/src/main/java/com/babyq/logisticsmanagement/service/UserService.java
@@ -1,12 +1,10 @@
 package com.babyq.logisticsmanagement.service;
 
-import cn.hutool.core.date.LocalDateTimeUtil;
 import com.babyq.logisticsmanagement.entity.User;
 import com.babyq.logisticsmanagement.mapper.UserMapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.stereotype.Service;
-import sun.util.resources.LocaleData;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
